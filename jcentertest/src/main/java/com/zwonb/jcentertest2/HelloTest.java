4c896ca68889660c62c1ea7e7a94eecd50719c4a
zwonb <1140114702@qq.com>
2018-10-27 11:20:20 +0800

    Initial commit

---
+++ jcentertest/src/main/java/com/zwonb/jcentertest2/HelloTest.java
@@ -0,0 +1,16 @@
+package com.zwonb.jcentertest2;
+
+import android.util.Log;
+
+/**
+ * github 仓库上传 jcenter
+ *
+ * @author zwonb
+ * @date 2018/10/27
+ */
+public class HelloTest {
+
+    public static void hello() {
+        Log.e("zwonb", "hello: 你好呀！");
+    }
+}
